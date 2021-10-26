26ffe01b1c80a27cdbf3107ed21be548064fa554
S F <moneybro@rambler.ru>
2021-10-26 16:21:11 +0300

    Добавьте файлы проекта.

---
+++ FenskeSergey_Skillbox_labs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FenskeSergey_Skillbox_labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lab001_001_Hello_world/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace Lab001_001_Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите 1 для HW через WriteLine, 2 - вывод HW через Write");
+            string choos = ReadLine();
+            switch (choos)
+            {
+                case "1":
+                    PrintHWInline();
+                    break;
+                case "2":
+                    PrintHWInColumn();
+                    break;
+                default:
+                    PrintHWInline();
+                    break;
+            }
+            ReadKey();
+        }
+
+        static void PrintHWInline()
+        {
+            WriteLine("Hello World! (with WriteLine)");
+        }
+
+        private static void PrintHWInColumn()
+        {
+            Write("Hello ");
+            Write("World ");
+            Write("!!! ");
+            Write("(with Write)");
+        }
+    }
+}
