26b999facc06399bde7b2780b190ef5689be5e8a
S F <moneybro@rambler.ru>
2021-10-26 16:21:11 +0300

    Удалены ненужные файлы.

---
+++ Lab001_001_Hello_world/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Lab001_001_Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите 1 для HW через WriteLine, 2 - вывод HW через Write");
+            string choos = ReadLine();
+            switch (choos)
+            {
+                case "1":
+                    PrintHwWithWriteline();
+                    break;
+                case "2":
+                    PrintHwWithWrite();
+                    break;
+                default:
+                    PrintHwWithWriteline();
+                    break;
+            }
+            ReadKey();
+        }
+        /// <summary>
+        /// метод для вывода через WriteLine
+        /// </summary>
+        static void PrintHwWithWriteline()
+        {
+            WriteLine("Hello World! (with WriteLine)");
+        }
+
+        /// <summary>
+        /// метод для вывода через Write
+        /// </summary>
+        private static void PrintHwWithWrite()
+        {
+            Write("Hello ");
+            Write("World ");
+            Write("!!! ");
+            Write("(with Write)");
+        }
+    }
+}
