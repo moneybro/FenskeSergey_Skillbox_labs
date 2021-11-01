5e06a7238b9e5fef2c8cb015c41ee49d5f63c820
S F <moneybro@rambler.ru>
2021-11-01 19:59:20 +0300

    В дз3 из исходников добавлено: 1. возможность изменять количество игроков 2. возможность изменения максимального шага уменьшения пирамиды.

--- Homework_Theme_03/Program.cs
+++ Homework_Theme_03/Program.cs
@@ -68,18 +68,135 @@ namespace Homework_Theme_03
             //
             // User2 победил!
 
-            StartApp();
+            StartGame();
         }
 
-        public static string[] gamers = new string[2];
-        public static int GameNumber { get; set; }
-        static void StartApp()
+        private static string[] gamers;
+        public static void setGamers(int gamersCount)
+        {
+            gamers = new string[gamersCount];
+        }
+
+        public static int GameNumber { get; set; } // изначальная высота пирамиды
+        private static int userTry = 4;             // значение шага по умолчанию
+        public static int UserTry {
+            get 
+            {
+                return userTry;
+            } 
+            set 
+            {
+                userTry = value;
+            }
+        } // величина шага userTry
+        static void StartGame()
+        {
+            StartGame(false);
+        }
+
+
+        static void StartGame(bool revenge)
         {
             Random random = new Random();
             GameNumber = random.Next(20, 120);
+            Clear();
             WriteLine("Вас приветствует игра Пирамида.");
-            WriteLine("Участвуют 2 игрока.");
-            
+
+            if (revenge)
+            {
+                printGamers();
+            }
+            else
+            {
+                newGamers();
+            }
+
+            WriteLine($"Высота пирамиды: {GameNumber}");
+
+            setDifficultyGame();
+
+            StartMoving();
+        }
+
+        private static void setDifficultyGame()
+        {
+            var inputPlace = new NewStruct(0, 0);
+            WriteLine("Выберете сложность игры:");
+            WriteLine("1 - стандартная игра, шаг от 1 до 4");
+            WriteLine("2 - Ввести свой шаг");
+            inputPlace.Item1 = CursorLeft;
+            inputPlace.Item2 = CursorTop + 1;
+            while (true)
+            {
+                bool gameDifficultChoose = int.TryParse(ReadKey().KeyChar.ToString(), out int gameDifficult);
+                if (gameDifficultChoose)
+                {
+                    switch (gameDifficult)
+                    {
+                        case 1:
+                            CursorLeft = 0;
+                            return;
+                        case 2:
+                            CursorLeft = 0;
+                            while (true)
+                            {
+                                Write("Введите максимальный размер шага, размер шага должен быть меньше высоты пирамиды: ");
+                                bool isInt = int.TryParse(ReadLine(), out int stepSize);
+                                if (isInt && stepSize >= 2)
+                                {
+                                    UserTry = stepSize;
+                                    return;
+                                }
+                                else
+                                {
+                                    WriteLine("Неверный ввод. Повторите");
+                                }
+                            }
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    SetCursorPosition(inputPlace.Item1, inputPlace.Item2 - 1);
+                    Write("Неверный ввод. Повторите");
+                    Write(new string(' ', WindowWidth));
+                    SetCursorPosition(inputPlace.Item1, inputPlace.Item2);
+                }
+            }
+        }
+
+        private static void printGamers()
+        {
+            Clear();
+            WriteLine("Итого участвуют игроки с никами:");
+            foreach (var gamer in gamers)
+            {
+                WriteLine(gamer);
+            }
+            WriteLine(new string('-', WindowWidth));
+        }
+
+        private static void newGamers()
+        {
+            while (true)
+            {
+                Write("Введите количество игроков:");
+                bool gamersCountIsInt = int.TryParse(ReadLine(), out int gamersCount);
+                if (gamersCountIsInt)
+                {
+                    setGamers(gamersCount);
+                    break;
+                }
+                else
+                {
+                    Write("Неверный ввод. Повторите");
+                    SetCursorPosition(0, 1);
+                    WriteLine(new string(' ', WindowWidth));
+                    SetCursorPosition(0, 1);
+                }
+            }
+
             for (int i = 0; i < gamers.Length; i++)
             {
                 gamers[i] = setGamersNicknames(i);
@@ -88,64 +205,10 @@ namespace Homework_Theme_03
 
             WriteLine(new string('-', WindowWidth));
 
-            WriteLine("Итого участвуют игроки с никами:");
-            foreach (var gamer in gamers)
-            {
-                WriteLine(gamer);
-            }
-            WriteLine(new string('-', WindowWidth));
-
-
-            WriteLine($"Высота пирамиды: {GameNumber}");
-
-            StartMoving();
-
-            ReadKey();
+            printGamers();
         }
 
-        private static void StartMoving()
-        {
-            int gameNumberTmp;
-            while (true)
-            {
-                for (int i = 0; i < gamers.Length; i++)
-                {
-                    bool wrongEnter = true;
-                    bool goodMove = false;
-                    do
-                    {
-                        Write($"Ходит игрок {gamers[i]}. Введи число от 1 до 4: ");
-                        goodMove = int.TryParse(ReadKey().KeyChar.ToString(), out int step);
-                        WriteLine();
-                        wrongEnter = !(goodMove && step >= 1 && step <= 4); // если правильный ввод, то выходим из цикала do while
-                        if (goodMove && step >= 1 && step <= 4)
-                        {
-                            CursorLeft = 0;
-
-                            gameNumberTmp = GameNumber - step;
-                            if (gameNumberTmp <= 0)
-                            {
-                                WriteLine($"Победил игрок {gamers[i]}!!! Ура. Качай его, ребята!");
-                                return;
-                            }
-                            else
-                            {
-                                WriteLine(step);
-                                WriteLine($"Высота пирамиды {GameNumber} - {step} = {gameNumberTmp}.");
-                                GameNumber = gameNumberTmp;
-                            }
-                            goodMove = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Неверный ввод!");
-                            continue;
-                        }
-                    }
-                    while (wrongEnter);
-                }
-            }
-        }
+        
 
         private static string setGamersNicknames(int gamerNumber)
         {
@@ -178,6 +241,93 @@ namespace Homework_Theme_03
             int i = 1;
             i = gamerNumber++;
             return tmpGamerName + i;
+        }
+
+        private static void StartMoving()
+        {
+            int gameNumberTmp;
+            while (true)
+            {
+                for (int i = 0; i < gamers.Length; i++)
+                {
+                    bool wrongEnter = true;
+                    bool goodMove = false;
+                    do
+                    {
+                        Write($"Ходит игрок {gamers[i]}. Введи число от 1 до {UserTry}: ");
+                        goodMove = int.TryParse(ReadLine(), out int step);
+                        wrongEnter = !(goodMove && step >= 1 && step <= UserTry); // если правильный ввод, то выходим из цикала do while
+                        if (goodMove && step >= 1 && step <= UserTry)
+                        {
+                            CursorLeft = 0;
+
+                            gameNumberTmp = GameNumber - step;
+                            if (gameNumberTmp <= 0)
+                            {
+                                WriteLine($"Победил игрок {gamers[i]}!!! Ура. Качай его, ребята!");
+                                WriteLine("Новая игра - нажмите 1, Реванш(те же игроки) - нажмите 0, другая кнопка - выход.");
+                                switch (ReadLine())
+                                {
+                                    case "0":
+                                        StartGame(true);
+                                        break;
+                                    case "1":
+                                        StartGame(false);
+                                        break;
+                                    default:
+                                        return;
+                                }
+                            }
+                            else
+                            {
+                                WriteLine($"Высота пирамиды {GameNumber} - {step} = {gameNumberTmp}.");
+                                GameNumber = gameNumberTmp;
+                            }
+                            goodMove = false;
+                        }
+                        else
+                        {
+                            WriteLine("Неверный ввод!");
+                            WriteLine();
+                            continue;
+                        }
+                    }
+                    while (wrongEnter);
+                }
+            }
+        }
+    }
+
+    internal struct NewStruct
+    {
+        public int Item1;
+        public int Item2;
+
+        public NewStruct(int item1, int item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is NewStruct other &&
+                   Item1 == other.Item1 &&
+                   Item2 == other.Item2;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1030903623;
+            hashCode = hashCode * -1521134295 + Item1.GetHashCode();
+            hashCode = hashCode * -1521134295 + Item2.GetHashCode();
+            return hashCode;
+        }
+
+        public void Deconstruct(out int item1, out int item2)
+        {
+            item1 = Item1;
+            item2 = Item2;
         }
     }
 }
